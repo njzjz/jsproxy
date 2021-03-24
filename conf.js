f55b10bb68222cec95f32acaea012f8cbc183d74
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-03-23 22:10:37 -0400

    GCE is down

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '116',
+  ver: '117',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -12,16 +12,10 @@ jsproxy_config({
 
   // 节点配置
   node_map: {
-    'gce-cn-tw': {
-      label: 'Google Cloud 中国台湾',
-      lines: {
-        'p2.njzjz.win:8443': 1,
-      },
-    },
     // 该节点用于加载大体积的静态资源
     'cfworker': {
-      label: '',
-      hidden: true,
+      label: 'CloudFlare Workers',
+      hidden: false,
       lines: {
         'p.njzjz.win': 1,
         // 收费版（高权重）
@@ -40,7 +34,7 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'gce-cn-tw',
+  node_default: 'cfworker',
   // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
